264f170bdc8fbeeb610e0b34b29923150d24f67c
Olly Dutton <Oliver.Dutton@news.com.au>
2016-11-24 18:07:25 +1100

    lion moves side to side

--- assets/js/lion.js
+++ assets/js/lion.js
@@ -3,11 +3,5 @@ document.body.onkeyup = function(e){
         document.querySelector('.roar').play();
         const lionClass = document.querySelector('.lion').classList;
         lionClass.toggle('show');
-      
-        const leftLeafClass = document.querySelector('.leaf.left').classList;
-        leftLeafClass.toggle('move');
-    
-        const rightLeafClass = document.querySelector('.leaf.right').classList;
-        rightLeafClass.toggle('move');
     }
 };
